ff63e33672650908bb6a6a1696677b209d0c85a9
Lisandrad <lisandraestefanid@gmail.com>
2022-09-12 16:55:08 -0400

    Creating Calculator

---
+++ index.js
@@ -0,0 +1,6 @@
+function sum() {
+    var one = document.getElementById("input1").value;
+    var two = document.getElementById("input2").value;
+       var sum = parseInt(one) + parseInt(two);
+     alert ("La suma es : " + sum)
+   }
